8605443e5afa49be8919381a9f2f40a415f46765
Michael Schultz <michaelwschultz@gmail.com>
2018-07-16 01:30:06 -0700

    App works! Adds buds listener to track database changes, adds sign out button and fixes registration

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 import firebase, { auth } from './firebase.js'
 
 export default class App extends React.Component {
   constructor() {
     super()
-    this.loginUser = this.loginUser.bind(this)
 
     this.state = {
       authed: false,
       buds: {},
-      me: {}
+      me: {},
+      name: '',
+      registered: false
     }
   }
 
@@ -28,12 +29,13 @@ export default class App extends React.Component {
         // User is signed in
         let me
         const authed = true
-        console.log(user.displayName + ' logged in')
+        console.log(user + ' authed')
 
         const eventref = db.ref('buds/')
         const snapshot = await eventref.once('value')
         const buds = snapshot.val()
 
+        console.log('AUTH BUDS ', buds, user.uid)
         if (buds.hasOwnProperty(user.uid)) {
           me = buds[user.uid]
           me.uid = user.uid // adds uid to user object
@@ -42,46 +44,44 @@ export default class App extends React.Component {
         this.setState({
           authed,
           buds,
-          me
+          me,
+          registered: true
         })
       } else {
         // User is signed out
         console.log('no user logged in')
 
         this.setState({
-          authed: false,
           buds: {},
-          me: {}
+          me: {},
+          registered: false
         })
       }
     })
+
+    // Setup listener for buds database
+    // This fires everytime the database changes
+    const buds = db.ref('buds/')
+    buds.on('value', function(snapshot) {
+      this.setState({
+        buds: snapshot.val()
+      })
+    }.bind(this))
   }
 
-  async buttonClickHandler(me) {
-    // const { buds } = this.state
+  signOut(me) {
+    // Use this to de-auth the user for real
+    // const auth = firebase.auth()
+    // auth.signOut().then(function() {
+    //   console.log('Signed out')
+    // }.bind(this), function(error) {
+    //   console.log('Problem signing out ', error)
+    // })
 
-    // if (!buds.hasOwnProperty(me.uid)) {
-    //   await this.setState(previousState => ({
-    //     buds: [...previousState.buds, me]
-    //   }))
-    // } else {
-    //   const buddies = [...buds]
-    //   const index = buddies.indexOf(me.displayName)
-    //   buddies.splice(index, 1)
-    //   await this.setState({
-    //     buds: buddies
-    //   })
-    // }
-
-    // if (budsObject) {
-      // Builds up array of firebase users
-      // for (const key of Object.keys(budsObject)) {
-      //   if (budsObject[key].status) {
-      //     buds.push(budsObject[key].name)
-      //   }
-      // }
-    // Update firebase
-    this.changeStatus(me)
+    // Soft sign out
+    this.setState({
+      registered: false
+    })
   }
 
   async changeStatus(me) {
@@ -95,8 +95,7 @@ export default class App extends React.Component {
     // Toggle status
     status = !status
     me.status = status
-    console.log('MY STATUS ', status)
-    
+
     db.ref('buds/' + me.uid).update({
       status
     }).then(
@@ -106,29 +105,51 @@ export default class App extends React.Component {
     )
   }
 
-  loginUser() {
+  registerUser(name) {
     const auth = firebase.auth()
-    const currentUser = auth.currentUser
+    const me = auth.currentUser
     const db = firebase.database()
 
-    db.ref('buds/' + currentUser.uid).set({
-      name: currentUser.name,
-      status: false
+    db.ref('buds/' + me.uid).set({
+      name,
+      status: false,
+      uid: me.uid
     })
+
     this.setState({
-      me: currentUser
+      me: {
+        name,
+        status: false,
+        uid: me.uid
+      },
+      name: '',
+      registered: true
     })
   }
 
-  render() {
-    const { authed, buds, me } = this.state
+  renderBuds() {
+    const { buds } = this.state
+    const budsArray = []
 
-    console.log('RENDER STATUS ', me.status)
+    for (const key of Object.keys(buds)) {
+      if (buds[key].status) {
+        budsArray.push(
+          <View style={styles.bud} key={`${buds[key].uid}-bud`}>
+            <Text>{buds[key].name}</Text>
+          </View>
+        )
+      }
+    }
+    return budsArray
+  }
+
+  render() {
+    const { authed, me, registered } = this.state
 
     if (authed) {
       return (
         <View style={styles.container}>
-          {me &&
+          {registered && me &&
             <View style={styles.headerArea}>
               <Text style={styles.name}>
                 Yo, {me.name}!
@@ -136,42 +157,52 @@ export default class App extends React.Component {
               <Text style={styles.headline}>
                 You down for games tonight?
               </Text>
+              <View style={styles.button}>
+                <Button
+                  onPress={e => this.signOut(me)}
+                  title='Sign out'
+                  color='#292C3F'
+                  accessibilityLabel='Tap to sign out'
+                />
+              </View>
             </View>
           }
 
           <View style={styles.buttonArea}>
-            {me ?
+            {registered && me ?
               <View style={styles.button}>
                 <Button
-                  onPress={e => this.buttonClickHandler(me)}
+                  onPress={e => this.changeStatus(me)}
                   title={me.status ? 'I’m Out' : 'I’m Down'}
                   color='#292C3F'
-                  accessibilityLabel='Click this button to let everyone know that your down for gaming tonight'
+                  accessibilityLabel='Tap to let everyone know that your down for gaming tonight'
                 />
               </View>
             :
-              <View style={styles.button}>
-                <Button
-                  onPress={e => this.loginUser()}
-                  title='Login'
-                  color='#292C3F'
-                  accessibilityLabel='Click this button to let everyone know that your down for gaming tonight'
+              <View>
+                <TextInput
+                  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                  onChangeText={(name) => this.setState({name})}
+                  defaultValue={this.state.name}
                 />
+                <View style={styles.button}>
+                  <Button
+                    onPress={e => this.registerUser(this.state.name)}
+                    disabled={!this.state.name}
+                    title='Register'
+                    color='#292C3F'
+                    accessibilityLabel='Click this button to let everyone know that your down for gaming tonight'
+                  />
+                </View>
               </View>
             }
           </View>
 
-          {/* me &&
+          {registered && me &&
             <View style={styles.budsArea}>
-              {buds.map(bud => {
-                return (
-                  <View style={styles.bud} key={bud}>
-                    <Text>{bud}</Text>
-                  </View>
-                )
-              })}
+              {this.renderBuds()}
             </View>
-          */}
+          }
         </View>
       )
     }
@@ -207,6 +238,11 @@ const styles = StyleSheet.create({
   buttonArea: {
     flex: 1,
     justifyContent: 'center',
+  },
+  nameInput: {
+   flex: 1,
+   justifyContent: 'center',
+   backgroundColor: '#292C3F'
   },
   button: {
     backgroundColor: '#F98C63',
